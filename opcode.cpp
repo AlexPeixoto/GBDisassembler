6deed047d358a717b680a3e18838bb615eb7b389
root <root@LAPTOP-IKLOTINK.localdomain>
2019-08-17 17:33:03 +0100

    Minor rework on single operand instruction. Added extra missing bottom instructions and reworked register mapping

--- opcode.cpp
+++ opcode.cpp
@@ -1,4 +1,5 @@
 #include "opcode.h"
+#include <sstream>
 
 Opcode::Opcode(){
     operationListTop = 
@@ -6,17 +7,17 @@ Opcode::Opcode(){
         {0x00, INSTRUCTION::NOP}, 
         {0x01, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::BC)}, 
         {0x02, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::BC), static_cast<int>(REG::A)}, 
-        {0x03, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::BC)}, 
-        {0x04, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::B)}, 
-        {0x05, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::B)}, 
+        {0x03, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::BC)}, 
+        {0x04, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::B)}, 
+        {0x05, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::B)}, 
         {0x06, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::B)}, 
         {0x07, INSTRUCTION::RLCA}, 
         {0x08, INSTRUCTION::LD, PARAMETER_TYPE::A16, PARAMETER_TYPE::REG, 0, static_cast<int>(REG::SP)}, 
         {0x09, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HL), static_cast<int>(REG::BC)}, 
         {0x0A, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::A), static_cast<int>(REG::BC)}, 
-        {0x0B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::BC)}, 
-        {0x0C, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::C)}, 
-        {0x0D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::C)}, 
+        {0x0B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::BC)}, 
+        {0x0C, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::C)}, 
+        {0x0D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::C)}, 
         {0x0E, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::C)}, 
         {0x0F, INSTRUCTION::RRCA},  
 
@@ -24,53 +25,125 @@ Opcode::Opcode(){
         {0x10, INSTRUCTION::STOP}, 
         {0x11, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::DE)}, 
         {0x12, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::DE), static_cast<int>(REG::A)}, 
-        {0x13, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::DE)}, 
-        {0x14, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::D)}, 
-        {0x15, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::D)}, 
+        {0x13, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::DE)}, 
+        {0x14, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::D)}, 
+        {0x15, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::D)}, 
         {0x16, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::D)}, 
         {0x17, INSTRUCTION::RLA}, 
         {0x18, INSTRUCTION::JR, PARAMETER_TYPE::R8}, 
         {0x19, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HL), static_cast<int>(REG::DE)}, 
         {0x1A, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::A), static_cast<int>(REG::DE)}, 
-        {0x1B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::DE)}, 
-        {0x1C, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::E)}, 
-        {0x1D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::E)}, 
+        {0x1B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::DE)}, 
+        {0x1C, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::E)}, 
+        {0x1D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::E)}, 
         {0x1E, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::E)}, 
         {0x1F, INSTRUCTION::RRA},  
 
         {0x20, INSTRUCTION::JR, PARAMETER_TYPE::JMP, PARAMETER_TYPE::R8, static_cast<int>(JMP::NZ)}, 
         {0x21, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::HL)}, 
         {0x22, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HLP), static_cast<int>(REG::A)}, 
-        {0x23, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::HL)}, 
-        {0x24, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::H)}, 
-        {0x25, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::H)}, 
+        {0x23, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
+        {0x24, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::H)}, 
+        {0x25, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::H)}, 
         {0x26, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::H)}, 
         {0x27, INSTRUCTION::DAA}, 
         {0x28, INSTRUCTION::JR, PARAMETER_TYPE::JMP, PARAMETER_TYPE::R8, static_cast<int>(JMP::Z)}, 
         {0x29, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HL), static_cast<int>(REG::HL)}, 
         {0x2A, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::A), static_cast<int>(REG::HLP)}, 
-        {0x2B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::HL)}, 
-        {0x2C, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::L)}, 
-        {0x2D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::L)}, 
+        {0x2B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
+        {0x2C, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::L)}, 
+        {0x2D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::L)}, 
         {0x2E, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::L)}, 
         {0x2F, INSTRUCTION::CPL},  
 
         {0x30, INSTRUCTION::JR, PARAMETER_TYPE::JMP, PARAMETER_TYPE::R8, static_cast<int>(JMP::NC)}, 
         {0x31, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::SP)}, 
         {0x32, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HLM), static_cast<int>(REG::A)}, 
-        {0x33, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::SP)}, 
-        {0x34, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::HL)}, 
-        {0x35, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::HL)}, 
+        {0x33, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::SP)}, 
+        {0x34, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
+        {0x35, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
         {0x36, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::HL)}, 
         {0x37, INSTRUCTION::SCF}, 
         {0x38, INSTRUCTION::JR, PARAMETER_TYPE::JMP, PARAMETER_TYPE::R8, static_cast<int>(JMP::C)}, 
         {0x39, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::HL), static_cast<int>(REG::SP)}, 
         {0x3A, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::A), static_cast<int>(REG::HLM)}, 
-        {0x3B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::SP)}, 
-        {0x3C, INSTRUCTION::INC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::A)}, 
-        {0x3D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::A)}, 
+        {0x3B, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::SP)}, 
+        {0x3C, INSTRUCTION::INC, PARAMETER_TYPE::REG, static_cast<int>(REG::A)}, 
+        {0x3D, INSTRUCTION::DEC, PARAMETER_TYPE::REG, static_cast<int>(REG::A)}, 
         {0x3E, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::A)}, 
         {0x3F, INSTRUCTION::CCF}
+    };
+
+    operationListBottom =
+    {
+        {0xC0, INSTRUCTION::RET, PARAMETER_TYPE::JMP, static_cast<int>(JMP::NZ)}, 
+        {0xC1, INSTRUCTION::POP, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::BC)}, 
+        {0xC2, INSTRUCTION::JP, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(JMP::NZ)}, 
+        {0xC3, INSTRUCTION::JP, PARAMETER_TYPE::A16}, 
+        {0xC4, INSTRUCTION::CALL, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(JMP::NZ)}, 
+        {0xC5, INSTRUCTION::PUSH, PARAMETER_TYPE::REG, PARAMETER_TYPE::NONE, static_cast<int>(REG::BC)}, 
+        {0xC6, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::A)}, 
+        {0xC7, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H00)}, 
+        {0xC8, INSTRUCTION::RET, PARAMETER_TYPE::REG, static_cast<int>(REG::Z)}, 
+        {0xC9, INSTRUCTION::RET}, 
+        {0xCA, INSTRUCTION::JP, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(REG::Z)}, 
+        //CB MAPPED SOMEWHERE ELSE
+        {0xCC, INSTRUCTION::CALL, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(REG::Z)}, 
+        {0xCD, INSTRUCTION::CALL, PARAMETER_TYPE::A16}, 
+        {0xCE, INSTRUCTION::ADC, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::A)}, 
+        {0xCF, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H08)}, 
+
+        {0xD0, INSTRUCTION::RET, PARAMETER_TYPE::JMP, static_cast<int>(JMP::NC)}, 
+        {0xD1, INSTRUCTION::POP, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::DE)}, 
+        {0xD2, INSTRUCTION::JP, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(JMP::NZ)}, 
+        {0xD3}, //INVALID 
+        {0xD4, INSTRUCTION::CALL, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(JMP::NC)}, 
+        {0xD5, INSTRUCTION::PUSH, PARAMETER_TYPE::REG, static_cast<int>(REG::DE)}, 
+        {0xD6, INSTRUCTION::SUB, PARAMETER_TYPE::D8}, 
+        {0xD7, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H10)}, 
+        {0xD8, INSTRUCTION::RET, PARAMETER_TYPE::REG, static_cast<int>(REG::C)}, 
+        {0xD9, INSTRUCTION::RETI}, 
+        {0xDA, INSTRUCTION::JP, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(REG::C)}, 
+        {0xDB}, //INVALID
+        {0xDC, INSTRUCTION::CALL, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(REG::C)}, 
+        {0xDD},  //INVALID
+        {0xDE, INSTRUCTION::SBC, PARAMETER_TYPE::REG, PARAMETER_TYPE::D8, static_cast<int>(REG::A)}, 
+        {0xDF, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H18)},   
+
+        {0xE0, INSTRUCTION::LDH, PARAMETER_TYPE::A8, PARAMETER_TYPE::REG, 0, static_cast<int>(REG::A)}, 
+        {0xE1, INSTRUCTION::POP, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::HL)}, 
+        {0xE2, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::C), static_cast<int>(REG::A)}, 
+        {0xE3}, //INVALID 
+        {0xE4}, //INVALID 
+        {0xE5, INSTRUCTION::PUSH, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
+        {0xE6, INSTRUCTION::AND, PARAMETER_TYPE::D8}, 
+        {0xE7, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H20)}, 
+        {0xE8, INSTRUCTION::ADD, PARAMETER_TYPE::REG, PARAMETER_TYPE::R8, static_cast<int>(REG::SP)}, 
+        {0xE9, INSTRUCTION::JP, PARAMETER_TYPE::REG, static_cast<int>(REG::HL)}, 
+        {0xEA, INSTRUCTION::LD, PARAMETER_TYPE::A16, PARAMETER_TYPE::REG, 0, static_cast<int>(REG::A)}, 
+        {0xEB}, //INVALID
+        {0xEC}, //INVALID 
+        {0xED}, //INVALID
+        {0xEE, INSTRUCTION::XOR, PARAMETER_TYPE::D8}, 
+        {0xEF, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H28)},
+
+        {0xF0, INSTRUCTION::LDH, PARAMETER_TYPE::REG, PARAMETER_TYPE::A8, static_cast<int>(REG::A)}, 
+        {0xF1, INSTRUCTION::POP, PARAMETER_TYPE::REG, PARAMETER_TYPE::D16, static_cast<int>(REG::AF)}, 
+        {0xF2, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::A), static_cast<int>(REG::C)}, 
+        {0xF3, INSTRUCTION::DI},
+        {0xF4}, //INVALID 
+        {0xF5, INSTRUCTION::PUSH, PARAMETER_TYPE::REG, static_cast<int>(REG::AF)}, 
+        {0xF6, INSTRUCTION::OR, PARAMETER_TYPE::D8}, 
+        {0xF7, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H30)},
+        //LD HL, SP+r8 (implicit from opcode), probably will be handled on a separate if 
+        {0xF8, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::R8, static_cast<int>(REG::HL)}, 
+        {0xF9, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::REG, static_cast<int>(REG::SP), static_cast<int>(REG::HL)}, 
+        {0xFA, INSTRUCTION::LD, PARAMETER_TYPE::REG, PARAMETER_TYPE::A16, static_cast<int>(REG::A)}, 
+        {0xFB, INSTRUCTION::EI},
+        {0xFC}, //INVALID 
+        {0xFD}, //INVALID
+        {0xFE, INSTRUCTION::CP, PARAMETER_TYPE::D8}, 
+        {0xFF, INSTRUCTION::RST, PARAMETER_TYPE::RST, static_cast<int>(RST::H38)}
     };
 }
 
@@ -78,24 +151,19 @@ Opcode::Opcode(){
 unsigned char* Opcode::generateInstruction(unsigned char* PC){
     unsigned char op = *PC;
 
-    //Those are simple instructions that I only print and return incrementing (those do not affect any register or need parameter)
-    switch(op){
-        case 0x00:
-            //NOP
-            return PC++;
-        case 0xCB:
-            PC++;
-            getCBInstruction(PC);
-            return PC++;
-        case 0x10:
-            //STOP 0
-            return PC++;
-        case 0x76:
-            //HALT
-            return PC++;
-        case 0xF3:
-            //DI
-            return PC++;
+    if(op == 0xCB){
+
+    }
+    else if(op >= 0x00 && op <= 0x3F){
+
+    }
+    else if(op >=0xC0 && op <= 0xFF){
+        int idx = op - 0xC0;
+        if(operationListBottom.at(idx).invalid){
+            std::stringstream errorMessage;
+            errorMessage << "Invalid opcode: " << std::hex << (int)op;
+            throw std::runtime_error(errorMessage.str());
+        }
     }
     
     //LD
@@ -107,7 +175,7 @@ unsigned char* Opcode::generateInstruction(unsigned char* PC){
         int op2Val = getRegisterFromLast4Bits<int>(op);
         op2 = regToString(op2Val);
         
-        //A similar rule applies for the first 4 bits, but I have to divide it by 8 assuming that it starts from 0x00
+        //A similar rule applies for the first 4 bits, but I have to divide it by 8 assuming that it starts from 0x00.
         int normalizedOp = op - 0x40; 
         //I know that after every 8 elements I move to the next register (normalized B starts at 0x00, c at 0x08, d at 0x10)
         int op1Val = normalizedOp/8;
@@ -148,11 +216,7 @@ unsigned char* Opcode::generateInstruction(unsigned char* PC){
         }
         return PC++;
     }
-    //Others
-    else{
-        //Use OperationList
-    }
-    
+    throw std::runtime_error("Could not match the opcode");
 }
 
 void Opcode::getCBInstruction(unsigned char *PC){}
--- opcode.h
+++ opcode.h
@@ -11,6 +11,25 @@ class Opcode
     enum class INSTRUCTION{
         NOP, LD, INC, DEC, RLCA, ADD, RRCA, STOP, RLA, JR, RRA, CPL, SCF, DAA, CCF, HALT, SUB, ADC, SBC, AND, XOR, OR, CP, RET, POP, JP, PUSH, RST, CB, CALL, RETI, LDH, DI, EI
     };
+    enum class CBINSTRUCTIONS{
+        RLC = static_cast<int>(INSTRUCTION::EI) + 1,
+        RRC,
+        RL, RR,
+        SLA, SRA,
+        SWAP, SRL,
+        BIT,/* BIT,
+        BIT, BIT,
+        BIT, BIT,
+        BIT, BIT,*/
+        RES,/* RES,
+        RES, RES,
+        RES, RES,
+        RES, RES,*/
+        SET/*, SET,
+        SET, SET,
+        SET, SET, 
+        SET, SET*/
+    };
 
     enum class PARAMETER_TYPE{
         NONE,
@@ -20,22 +39,23 @@ class Opcode
         A16,
         R8,
         REG,
-        JMP
+        JMP,
+        RST
     };
     enum class REG{
-        B,
-        C,
-        D,
-        E,
-        H,
-        L,
+        B, C,
+        BC,
+        D, E,
+        DE,
+        H, L,
         HL,
         HLP,
         HLM,
-        A,
-        BC,
-        SP,
-        DE
+
+        A, F, //F IS NOT ALWAYS USED, IT CONTAINS ALL THE FLAGS (ZNHC)
+        AF,
+        Z,
+        SP
     };
 
     //It is passed as a parameter (type JUMP)
@@ -44,7 +64,18 @@ class Opcode
         Z,
         NC,
         C
+    };
 
+    //Maps parameters for RST calls
+    enum class RST{
+        H00 = (static_cast<int>(JMP::C) + 1),
+        H10,
+        H20,
+        H30,
+        H08,
+        H18,
+        H28,
+        H38
     };
     //Reg to string
     const std::vector<std::string> registers = {"B", "C", "D", "E", "H", "L", "HL", "A"};
@@ -54,14 +85,25 @@ class Opcode
     struct Operations{
         Operations(uint16_t opcode, INSTRUCTION instruction,
                    PARAMETER_TYPE type_1 = PARAMETER_TYPE::NONE, PARAMETER_TYPE type_2 = PARAMETER_TYPE::NONE,
-                   uint16_t parameter_1 = 0, uint16_t parameter_2 =0){
+                   uint16_t parameter_1 = 0, uint16_t parameter_2 = 0){
             this->opcode=opcode;
             this->instruction = instruction;
             this->type[0] = type_1;
             this->type[1] = type_2;
             this->parameter[0] = parameter_1;
             this->parameter[1] = parameter_2;
+            this->invalid = false;
         }
+
+        Operations(uint16_t opcode, INSTRUCTION instruction,
+                   PARAMETER_TYPE type_1,  uint16_t parameter_1) : Operations(opcode, instruction, type_1, PARAMETER_TYPE::NONE, parameter_1, 0) {};
+        
+        //When only opcode is supplied, its marked as invalid operation
+        Operations(uint16_t opcode){
+            this->opcode = opcode;
+            this->invalid = true;
+        }
+
         uint16_t opcode;
         INSTRUCTION instruction;
         std::string description;

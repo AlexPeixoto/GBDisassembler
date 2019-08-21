8824eab96271e9a431e87cf276fdb9fd2b860191
Alex <lefsler@outlook.com>
2019-08-21 11:53:05 +0100

    Remove unecessary files

--- stringifyOperation.h
+++
@@ -1,7 +0,0 @@
-/*
-    std::string regToString(int _r) const{
-        return registers[_r];
-    }
-        //Reg to string
-    const std::vector<std::string> registers = {"B", "C", "D", "E", "H", "L", "HL", "A"};
-    */
